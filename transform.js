a9856c8f0edb4b4afca788c4375f5271a225eafa
Ayanava <32432911+terrible-coder@users.noreply.github.com>
2019-01-14 12:39:21 +0530

    Revert "on way to getting image outline"

--- transform.js
+++ transform.js
@@ -8,11 +8,11 @@ for(let k = 0; k < N; k++) {
 	list.push(Complex.from_cartesian(a*Math.cos(t)+50*Math.random(), b*Math.sin(t)+50*Math.random()));
 	t += 2*Math.PI/N;
 }
-// console.log(list);
+console.log(list);
 
 // transforming
 const w = Complex.from_polar(1, -2*Math.PI/N);
-// console.log(w);
+console.log(w);
 // calculating coefficients
 const coeffs = [];
 for(let k = 0; k < N; k++) {
@@ -34,4 +34,4 @@ const root = cycles[0];
 for(let k = 1; k < cycles.length; k++) {
 	Epicycle.add_child(cycles[k-1], cycles[k]);
 }
-// console.log(cycles);
+console.log(cycles);
