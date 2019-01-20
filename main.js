8a22fc6bc7e5b4e5392484655f930704ab95febf
terrible-coder <ayanavade01@gmail.com>
2019-01-20 18:45:43 +0530

    Adding additional tweaks to movement of circles on screen.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const width = 600;
 const height = 600;
-
+let speed = 1;
 let path = [];
 
 const canvas = document.createElement("canvas");
@@ -10,6 +10,8 @@ const context = canvas.getContext("2d");
 document.getElementById("display").append(canvas);
 
 document.getElementById("file_list").onchange = event => {
+	document.getElementById("loading").style.display = "block";
+	canvas.style.display = "none";
 	const file = event.target.files[0];
 	if(!file.type.match("image.*"))
 		throw "File must be an image file."
@@ -25,6 +27,7 @@ document.getElementById("file_list").onchange = event => {
 }
 
 function image_loaded() {
+	document.getElementById("loading").style.display = "none";
 	console.log("image loaded");
 	// preprocessing data
 	let image = get_image_data(this, width, height);
@@ -39,6 +42,8 @@ function image_loaded() {
 	console.log(boundary.length);
 	let shortest = shortest_path(boundary);
 	reduce(shortest);
+	const origin = mean(shortest);
+	shortest = offset(shortest, origin);
 	console.log(shortest);
 	console.log("calculating...");
 	// transforming to frequency space
@@ -48,25 +53,35 @@ function image_loaded() {
 	Fourier.transform(obj);
 	Fourier.create_epicycles(obj);
 	const avg_ang_speed = 0.01;
+	let period = 0;
 	let now = 0;
 	console.log("Done.");
 	canvas.style.display = "inline-block";
 	(function animate(time = 0) {
+		const resolution = 2;
 		const dt = (time - now) / 1000;
 		now = time;
 		if(!paused) {
 			period += dt;
 			context.resetTransform();
 			context.fillStyle = "#ffffff";
+			context.globalAlpha = 1.0;
 			context.fillRect(0, 0, width, height);
+			context.translate(Complex.real(origin), Complex.imag(origin));
 			context.fillStyle = "#000000";
+			if(period*avg_ang_speed > 4*Math.PI) {
+				period = 0;
+				path = [];
+			}
 			const loop = Math.abs(avg_ang_speed / obj.root_cycle.angular_speed) | 0;
+			const iterations = loop * resolution * speed;
 			// console.log(loop);
-			for(let i = 0; i < loop; i++) {
+			for(let i = 0; i < iterations; i++) {
 				const end = Fourier.step(obj, dt/resolution);
 				trace(Complex.real(end), Complex.imag(end));
 			}
 			path.forEach(point => context.fillRect(point.x, point.y, 2, 2));
+			context.globalAlpha = 0.2;
 			context.save();
 			Fourier.draw(context, obj);
 			context.restore();
